b3a2d42a5e8f0e531052ebdce55985989161a739
arllain <arllain@gmail.com>
2019-03-04 09:56:20 -0300

    Creation of the SearchInput component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, KeyboardAvoidingView, Platform } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import SearchInput from './components/SearchInput';
 
 
 export default class App extends React.Component {
@@ -10,13 +11,7 @@ export default class App extends React.Component {
           <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
           <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
           <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-
-          <TextInput autoCorrect={false}
-            placeholder="Search any city"
-            placeholderTextColor="white"
-            style={styles.TextInput}
-            clearButtonMode="always"
-          />
+          <SearchInput placeholder="Search any city"/>
       </KeyboardAvoidingView>
     );
   }
@@ -40,15 +35,4 @@ const styles = StyleSheet.create({
   smallText: {
     fontSize: 18,
   },
-TextInput:{
-  backgroundColor: '#666',
-  color: 'white',
-  height: 40,
-  width: 300,
-  marginTop: 20,
-  marginHorizontal: 20,
-  paddingHorizontal: 10,
-  alignSelf: 'center',
-},
-
 });
